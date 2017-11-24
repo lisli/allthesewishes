6a9ea809053c2db6e0944282f2087a431f419d19
Chad Ostrowski <hi@chadoh.com>
2017-11-24 10:27:45 -0500

    PouchDB: remove uuid
    If we use `db.put` to create items, we need to manually set the `_id`
    field. But if we use `db.post`, PouchDB creates the `_id` for us.
    Internally, it also uses `uuid`. This means we don't need to worry about
    this logic ourselves anymore.

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import uuid from 'uuid/v4';
 import PouchDB from 'pouchdb';
 
 const db = new PouchDB('wishlist');
